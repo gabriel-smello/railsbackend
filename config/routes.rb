d9e713b007c3f4ea66a94f8aec7cebdb103724cc
gabriel <gabrielhsm1402@gmail.com>
2022-04-28 17:03:15 -0300

    v0.1

---
+++ config/routes.rb
@@ -0,0 +1,25 @@
+Rails.application.routes.draw do
+  resources :notes
+
+  resource :user, only: [:create]
+  post '/login', to: 'user#login'
+  get '/auto_login', to: 'user#auto_login'
+
+  namespace 'api' do 
+    namespace 'v1' do 
+      scope 'user' do
+        get 'index', to: 'user#index'
+        get 'show/:id', to: 'user#show'
+        post 'create', to: 'user#create'
+        
+      end
+
+      scope 'music' do 
+        get 'index', to: 'music#index'
+        get 'order', to: 'music#order'
+        get 'show/:id', to: 'music#show'
+        
+      end
+    end
+  end
+end
